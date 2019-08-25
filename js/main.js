81bdf4b2f075ccdf07aeaea967ef609aa26f041e
Ashutosh Singh <ashutosh.singh@mindfiresolutions.com>
2019-08-25 12:09:51 +0530

    Pushing changes of loader
    Pushing changes of loader

--- js/main.js
+++ js/main.js
@@ -3,5 +3,12 @@ $(document).ready(function(){
         $(".menu-wrapper").toggleClass("toggle-me");  
         $(".menu-content-wrapper").toggleClass("toggle-menu");  
         $("body").toggleClass("overflow-hidden");  
-    });  
+    }); 
+    $(".section-loader").click(function() {
+
+    }) 
+    setTimeout(function() {
+        $('.section-loader').addClass('loader-hidden');
+    },5000);
 });  
+
