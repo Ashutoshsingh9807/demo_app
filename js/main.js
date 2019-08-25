4a5b11f08b030a296100c23ed51c9154a7963bd0
Ashutosh Singh <ashutosh.singh@mindfiresolutions.com>
2019-08-25 19:59:42 +0530

    Fixed loader issue.
    Fixed loader issue.

--- js/main.js
+++ js/main.js
@@ -4,27 +4,11 @@ $(document).ready(function(){
         $(".menu-content-wrapper").toggleClass("toggle-menu");  
         $("body").toggleClass("overflow-hidden");  
     }); 
-    $(".section-loader").click(function() {
-
-    }) 
+    
     setTimeout(function() {
         $('.section-loader').addClass('loader-hidden');
-    },5000);
-    /*circlular text function */
-    // function circularText(txt, radius, classIndex) {
-    //     txt = txt.split(""),
-    //       classIndex = document.getElementsByClassName("circTxt")[classIndex];
-      
-    //     var deg = 360 / txt.length,
-    //       origin = 0;
-      
-    //     txt.forEach((ea) => {
-    //       ea = `<p style='height:${radius}px;position:absolute;transform:rotate(${origin}deg);transform-origin:0 100%'>${ea}</p>`;
-    //       classIndex.innerHTML += ea;
-    //       origin += deg;
-    //     });
-    //   }
-      
-    //   circularText("this text is in a circle  with name and function ", 270, 0);
+        $("body").removeClass("overflow-hidden"); 
+    },3000);
+   
 });  
 
